fdde6b42b72ed2e632cfdfb181bdc37c75400128
Michelle <mliuu3259@gmail.com>
2022-11-22 10:51:39 -0500

    replies

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,9 @@
+// options = perspectiveTaking, greaterGood
+let method = "perspectiveTaking";
+
+// options = bot, user
+let mode = "bot";
+
 const svg =
   '<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit css-1cw4hi4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InfoOutlinedIcon"><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20, 12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10, 10 0 0,0 12,2M11,17H13V11H11V17Z"></path></svg>';
 const config = [
@@ -6,20 +12,39 @@ const config = [
     id: "t1_iwp3oty",
     explanation:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    replies: [],
+    replies: ["hi this is a reply", "do better"],
   },
   {
     page: "r/cmu/comments/yxk0p7/what_do_you_think_of_the_future_of_the_tech/",
     id: "t1_iwq89qz",
-    explanation: "hiiiiii",
-    replies: [],
+    explanation: "this is a mean post",
+    replies: ["another sample reply", "this was really rude. stop."],
   },
 ];
-const injectBlock = ({ id, explanation }) => {
+const injectBlock = ({ id, explanation, replies }) => {
   const ele = document.getElementById(id);
   const newEle = document.createElement("div");
+  const replyBlocks = replies.map((reply) => {
+    return `
+      <div class="explanation-reply">
+        <p>${reply}</p>
+      </div>`;
+  });
+  console.log(replyBlocks.join(""));
   newEle.className = "explanation-block";
-  newEle.innerHTML = `<div class="svg-wrapper">${svg}</div><p>${explanation}</p>`;
+  newEle.innerHTML = `
+    <div class="svg-wrapper">
+      ${svg}
+    </div>
+    <br />
+    <div class="explanation-column">
+      <p>${explanation}</p>
+      <br />
+      <p>You can make the internet a better place by educating the user about why their reply is harmful. Choose from one of the prompts to post a reply from ${
+        mode === "bot" ? "a bot" : "your account"
+      }:</p>
+      ${replyBlocks.join("")}
+    </div>`;
   if (ele) ele.appendChild(newEle);
   console.log(id, ele, newEle);
 };
