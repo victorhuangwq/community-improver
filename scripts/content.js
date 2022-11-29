c5ea894fc412b5485e74bb4ae2544e253bb9a9d1
Michelle <mliuu3259@gmail.com>
2022-11-29 11:00:55 -0500

    confg update

--- scripts/content.js
+++ scripts/content.js
@@ -8,8 +8,8 @@ const svg =
   '<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit css-1cw4hi4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InfoOutlinedIcon"><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20, 12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10, 10 0 0,0 12,2M11,17H13V11H11V17Z"></path></svg>';
 const config = [
   {
-    page: "r/cmu/comments/yxk0p7/what_do_you_think_of_the_future_of_the_tech/",
-    id: "t1_iwp3oty",
+    page: "what_sin_had_i_committed_in_my_past_life_that_i",
+    id: "t1_ix28kvg",
     explanation:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     replies: ["hi this is a reply", "do better"],
@@ -20,9 +20,22 @@ const config = [
     explanation: "this is a mean post",
     replies: ["another sample reply", "this was really rude. stop."],
   },
+  {
+    page: "r/AskMen/comments/yza1z7/how_does_it_make_you_feel_when_you_know_that/",
+    id: "t1_iwz0htv",
+    explanation: "this is a mean post",
+    replies: ["another sample reply", "this was really rude. stop."],
+  },
+  {
+    page: "r/AskMen/comments/yza1z7/how_does_it_make_you_feel_when_you_know_that/",
+    id: "t1_iwz4md4",
+    explanation: "this is really sexist.",
+    replies: ["another sample reply", "this was really rude. stop."],
+  },
 ];
 const injectBlock = ({ id, explanation, replies }) => {
   const ele = document.getElementById(id);
+  console.log(ele);
   const newEle = document.createElement("div");
   const replyBlocks = replies.map((reply) => {
     return `
@@ -30,7 +43,6 @@ const injectBlock = ({ id, explanation, replies }) => {
         <p>${reply}</p>
       </div>`;
   });
-  console.log(replyBlocks.join(""));
   newEle.className = "explanation-block";
   newEle.innerHTML = `
     <div class="svg-wrapper">
@@ -47,7 +59,6 @@ const injectBlock = ({ id, explanation, replies }) => {
       ${replyBlocks.join("")}
     </div>`;
   if (ele) ele.appendChild(newEle);
-  console.log(id, ele, newEle);
 };
 
 function injectCSS() {
@@ -58,14 +69,20 @@ function injectCSS() {
   document.getElementsByTagName("head")[0].appendChild(link);
 }
 
-window.onload = () => {
-  injectCSS();
+const injectComments = () => {
+  console.log("inject");
   config
     .filter(({ page }) => {
       console.log(window.location.href);
       return window.location.href.includes(page);
     })
     .forEach(injectBlock);
+};
+
+window.onload = () => {
+  injectCSS();
+  injectComments();
+  // document.querySelector(".DraftEditor-root").onclick = injectComments;
 };
 
 // // `document.querySelector` may return null if the selector doesn't match anything.
