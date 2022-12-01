3d6dc0e264fa5b23b02d539c40a0dad1b0eb95e8
Victor Huang <victorhuangwq@gmail.com>
2022-11-30 22:20:54 -0500

    Adds delay to code

--- scripts/content.js
+++ scripts/content.js
@@ -4,18 +4,21 @@ let method = "perspectiveTaking";
 // options = bot, user
 let mode = "user";
 
+const delayInMilliseconds = 200 + Math.random() * 100; //delay to allow for the comment to be posted
+
 const config = [];
 
 const svg =
   '<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit css-1cw4hi4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InfoOutlinedIcon"><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20, 12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10, 10 0 0,0 12,2M11,17H13V11H11V17Z"></path></svg>';
 
-const getParent = (id) => {
+// Gets second div inside commentDiv
+const getSecondDiv = (id) => {
   const commentDiv = document.querySelector(`#${id} > .Comment`);
-
   // Get second div inside commentDiv
   return commentDiv.children[2];
 };
 
+// initializes data from data.js
 const initialize = () => {
   data.forEach((comment) => {
     const object = {};
@@ -37,13 +40,25 @@ const initialize = () => {
   });
 }
 
-const injectBlock = ({ id, explanation, replies, replacement_text }) => {
-  // Get second div inside commentDiv
-  const ele = getParent(id);
+// Injects CSS into the page
+const injectCSS = () => {
+  const link = document.createElement("link");
+  link.href = chrome.runtime.getURL("style.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
 
+// Injects a block of comments into the page for each comment in the config
+const injectBlock = ({ id, explanation, replies, replacement_text }) => {
+
+  // Get second div inside commentDiv
+  const ele = getSecondDiv(id);
   const text = ele.querySelector(".RichTextJSON-root").querySelector("p");
+
   console.log("text", text);
   text.innerHTML = replacement_text ?? text.innerHTML;
+
   const newEle = document.createElement("div");
   const replyBlocks = replies.map((reply) => {
     const replyEle = document.createElement("div");
@@ -54,6 +69,7 @@ const injectBlock = ({ id, explanation, replies, replacement_text }) => {
     };
     return replyEle;
   });
+
   newEle.className = "explanation-block";
   newEle.innerHTML = `
     <div class="svg-wrapper">
@@ -69,6 +85,7 @@ const injectBlock = ({ id, explanation, replies, replacement_text }) => {
         }:</p>
         <div id="replies"></div>
     </div>`;
+  
   if (ele) {
     ele.appendChild(newEle);
     replyBlocks.forEach((reply) => {
@@ -76,14 +93,6 @@ const injectBlock = ({ id, explanation, replies, replacement_text }) => {
     });
   }
 };
-
-function injectCSS() {
-  const link = document.createElement("link");
-  link.href = chrome.runtime.getURL("style.css");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  document.getElementsByTagName("head")[0].appendChild(link);
-}
 
 const injectComments = () => {
   console.log("inject");
@@ -107,7 +116,7 @@ const commentGenerator = (avatar, userName, comment_id, comment) => {
   const commentDiv = document.querySelector(`#${comment_id} > .Comment`);
 
   // Get second div inside commentDiv
-  const parent = getParent(comment_id);
+  const parent = getSecondDiv(comment_id);
   const cloneDiv = commentDiv.cloneNode(true);
   cloneDiv.querySelector(".explanation-block").remove();
   cloneDiv.querySelectorAll(".inserted-comment").forEach((comment) => {
@@ -157,14 +166,16 @@ const addComment = (parent_comment_id, comment) => {
   console.log("comment", comment);
   const userDropdown = document.querySelector("#USER_DROPDOWN_ID");
   const avatar = userDropdown.querySelector('img[alt="User avatar"]');
+  
   // Getting user name, get first span of #email-collection-tooltip-id
   const userName = document
     .querySelector("#email-collection-tooltip-id")
     .querySelector("span")
     .querySelector("span").innerText;
-  commentGenerator(avatar, userName, parent_comment_id, comment);
-};
+  
+  // Adds delay to make it feel real
+  setTimeout(function() {
+    commentGenerator(avatar, userName, parent_comment_id, comment);
+  }, delayInMilliseconds);
 
-// setTimeout(() => {
-//   addComment("t1_iwpmzi3", "comment");
-// }, 3000);
+};
